b47f08541ed519ea81ce134a4febd9f4d4f37b58
Orion Hu <chinax5132@163.com>
2016-06-06 22:59:56 +0800

    develop security function base 2.4.4; enhanced filter functions;

--- src/output/fileoutput.c
+++ src/output/fileoutput.c
@@ -19,31 +19,49 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  */
+ 
+/********************************************************************************************
+* Copyright @ Huawei Technologies Co., Ltd. 1998-2016. All rights reserved.  
+* File name: fileoutput.h
+* History:   
+*     1. Date: 2016/4/26
+*         Author: HuXinlei
+*		  Modification: employ Inter-Process Communication (IPC) techniques, 
+*            in order to not grant the specialed log directory and files write pemission to 
+*			non-root users any more.
+*********************************************************************************************
+*/
 
+ 
 
-
-/*
- * Includes order: from local to global (local, this-subsystem, snoopy.h, other-subsystems, global)
- */
 #include "fileoutput.h"
-
 #include "snoopy.h"
 #include "configuration.h"
-
+#include "securec.h" 
+#include <sys/msg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/msg.h>
+#include <unistd.h>
 
+
+#define MAX_TEXT 4096
+struct msg_st
+{
+	long int msg_type;
+	char text[MAX_TEXT];
+};
 
 
 /*
  * SNOOPY OUTPUT: fileoutput (called like this because <socket.h> is system library
  *
  * Description:
- *     Sends given message to socket
+ *     Sends given message to message queue
  *
  * Params:
- *     message:    message to send
+ *     logMessage: message to send
  *     errOrMsg:   is message and error message or ordinary Snoopy log message
  *     arg:        output argument(s)
  *
@@ -52,22 +70,37 @@
  */
 int snoopy_output_fileoutput (char const * const logMessage, int errorOrMessage, char const * const arg)
 {
-    FILE  *fp;
-    int    charCount;
-
-    // Check if output file is properly configured
-    if (0 == strcmp(arg, "")) {
-        return SNOOPY_OUTPUT_FAILURE;
-    }
-
-    // Try to open file in append mode
-    fp = fopen(arg, "a");
-    if (NULL == fp) {
-        return SNOOPY_OUTPUT_FAILURE;
-    }
-
-    // Try to print to file
-    charCount = fprintf(fp, "%s\n", logMessage);
-    fclose(fp);
-    return charCount;
+ 
+	int msgid = -1;
+	
+	struct msg_st data;
+	
+	char name[MAX_TEXT];
+	char *tmp = "&&";
+	char *enter = "\n\r";
+	
+	
+	//create message queue
+	msgid = msgget((key_t)1234, 0666 | IPC_CREAT);
+	if(msgid == -1)
+	{
+		return SNOOPY_OUTPUT_FAILURE;
+	}
+	
+	//input data
+	data.msg_type = 1;    
+	strcat_s(name,MAX_TEXT,arg);
+	strcat_s(name,MAX_TEXT,tmp);
+	strcat_s(name,MAX_TEXT,logMessage);
+	strcat_s(name,MAX_TEXT,enter);
+	strcpy_s(data.text, MAX_TEXT, name);
+	//send message to queue
+	if(msgsnd(msgid, (void*)&data, MAX_TEXT, IPC_NOWAIT) == -1)
+	{
+		return SNOOPY_OUTPUT_FAILURE;
+	}
+		
+	memset_s(data.text,MAX_TEXT,0,strlen(data.text));
+	memset_s(name,MAX_TEXT,0,strlen(name));
+	return strlen(logMessage);
 }
